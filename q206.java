40a2fbf8353179e24097d02f4dbd56705f4d9679
Tahmid Sajin <64264899+tahmid-saj@users.noreply.github.com>
2023-06-20 00:29:48 -0400

    Create q206.java

---
+++ q206.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode prev = null, curr = head, next = head.next;
+
+        while (curr != null) {
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+
+            if (next != null) {
+                next = next.next;
+            }
+        }
+
+        return prev;
+    }
+}
